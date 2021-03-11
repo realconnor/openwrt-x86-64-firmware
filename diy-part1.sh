aca2abc4fcd16627370830151334a661e70114a3
realconnor <zgtthu@gmail.com>
2021-03-11 21:24:15 +0800

    add extra packages repos.

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,9 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Luci for chinadns-ng
+git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
+# Extra packages
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
